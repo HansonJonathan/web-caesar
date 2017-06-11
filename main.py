0697d8a944a738ab306eb48b25cdbf4b17065af8
HansonJonathan <hansonjonathan@outlook.com>
2017-06-11 02:47:08 -0700

    updated main.py so result is placed in textarea

--- main.py
+++ main.py
@@ -5,47 +5,47 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-form = '''
+form = """
 <!DOCTYPE html>
 <html>
 	<head>
 		<style>
-			form {
+			form {{
 				background-color: #eee;
 				padding: 20px;
 				margin: 0 auto;
 				width: 540px;
 				font: 16px sans-serif;
 				border-radius: 10px;
-			}
-			textarea {
+			}}
+			textarea {{
 				margin: 10px 0;
 				width: 540px;
 				height: 120px;
-			}
+			}}
 		</style>
 	</head>
 	<body>
 		<form method="POST">
-			<label for="rot">Rotate by:</label>
-			<input type="text" name="rot" value="0" />
-			<textarea name="text"></textarea>
-			<input type="submit" value="Submit Query">
+				<label for="rot">Rotate by:</label>
+				<input type="text" name="rot" value="0" />
+				<textarea name="text">{0}</textarea>
+				<input type="submit" value="Submit Query">
 		</form>
 	</body>
 </html>
-'''
+"""
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
 	form_rot = int(request.form['rot'])
 	form_text = request.form['text']
 	encrypted = rotate_string(form_text,form_rot)
-	return "<h1>{}</h1>".format(encrypted)
+	return form.format(encrypted)
 
 
 app.run()
